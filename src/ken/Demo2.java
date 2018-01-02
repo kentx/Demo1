294a594996fcd4130a7a8d4a5547bf9b2f55f7e8
kentx <962605656@qq.com>
2018-01-02 11:16:59 +0800

    first

---
+++ src/ken/Demo2.java
@@ -0,0 +1,17 @@
+package ken;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("ฮาบร");
+        int a;
+        int b;
+        int result=add(5,9);
+        System.out.println(result);
+	}
+
+	public static int add(int c,int d){
+		return c+d;
+	}
+}
